d1a58001f27f3cdf10e870ae898767b22b2c543d
Charles <charlo22@hotmail.fr>
2021-05-13 18:02:50 +0200

    Feature: Contact section css+js

--- build/app.js
+++ build/app.js
@@ -1,6 +1,20 @@
 const nav = document.querySelector("#navMobileMenu");
 const hamburger = document.querySelector("#hamburger");
 
+const clip = document.querySelectorAll("#clip")
+
 hamburger.addEventListener("click", () => {
     nav.classList.toggle("hidden");
+});
+
+clip.forEach( (button) => {
+    button.addEventListener("click", () => {
+        const clip = button.parentElement.querySelector("a").innerText;
+        const icon = button.firstElementChild
+        navigator.clipboard.writeText(clip).then(() => {
+            icon.classList.value = "fas fa-clipboard-check";
+          }, () => {
+            icon.classList.value = "fas fa-times text-red-600";
+          });
+    })
 });
